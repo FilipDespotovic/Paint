75c48200c0ffe4a75c60ed55a7875c0e0d44a0f8
Jelena Lucic <lucic.jelena00@gmail.com>
2018-07-07 13:20:20 +0200

    pocetak

---
+++ WindowsFormsApp5/WindowsFormsApp5/Form1.Designer.cs
@@ -0,0 +1,185 @@
+﻿namespace WindowsFormsApp5
+{
+    partial class Paint
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.olovkaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.gumicaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.velicinaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.uveziFotografijuToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.sačuvajToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.bojaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.crvenaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.plavaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.zelenaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.žutaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem2 = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem3 = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem4 = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.olovkaToolStripMenuItem,
+            this.gumicaToolStripMenuItem,
+            this.velicinaToolStripMenuItem,
+            this.uveziFotografijuToolStripMenuItem,
+            this.sačuvajToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // olovkaToolStripMenuItem
+            // 
+            this.olovkaToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.bojaToolStripMenuItem});
+            this.olovkaToolStripMenuItem.Name = "olovkaToolStripMenuItem";
+            this.olovkaToolStripMenuItem.Size = new System.Drawing.Size(56, 20);
+            this.olovkaToolStripMenuItem.Text = "Olovka";
+            // 
+            // gumicaToolStripMenuItem
+            // 
+            this.gumicaToolStripMenuItem.Name = "gumicaToolStripMenuItem";
+            this.gumicaToolStripMenuItem.Size = new System.Drawing.Size(60, 20);
+            this.gumicaToolStripMenuItem.Text = "Gumica";
+            // 
+            // velicinaToolStripMenuItem
+            // 
+            this.velicinaToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItem2,
+            this.toolStripMenuItem3,
+            this.toolStripMenuItem4});
+            this.velicinaToolStripMenuItem.Name = "velicinaToolStripMenuItem";
+            this.velicinaToolStripMenuItem.Size = new System.Drawing.Size(59, 20);
+            this.velicinaToolStripMenuItem.Text = "Velicina";
+            // 
+            // uveziFotografijuToolStripMenuItem
+            // 
+            this.uveziFotografijuToolStripMenuItem.Name = "uveziFotografijuToolStripMenuItem";
+            this.uveziFotografijuToolStripMenuItem.Size = new System.Drawing.Size(106, 20);
+            this.uveziFotografijuToolStripMenuItem.Text = "Uvezi fotografiju";
+            // 
+            // sačuvajToolStripMenuItem
+            // 
+            this.sačuvajToolStripMenuItem.Name = "sačuvajToolStripMenuItem";
+            this.sačuvajToolStripMenuItem.Size = new System.Drawing.Size(59, 20);
+            this.sačuvajToolStripMenuItem.Text = "Sačuvaj";
+            // 
+            // bojaToolStripMenuItem
+            // 
+            this.bojaToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.crvenaToolStripMenuItem,
+            this.plavaToolStripMenuItem,
+            this.zelenaToolStripMenuItem,
+            this.žutaToolStripMenuItem});
+            this.bojaToolStripMenuItem.Name = "bojaToolStripMenuItem";
+            this.bojaToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.bojaToolStripMenuItem.Text = "Boja";
+            // 
+            // crvenaToolStripMenuItem
+            // 
+            this.crvenaToolStripMenuItem.Name = "crvenaToolStripMenuItem";
+            this.crvenaToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.crvenaToolStripMenuItem.Text = "crvena";
+            // 
+            // plavaToolStripMenuItem
+            // 
+            this.plavaToolStripMenuItem.Name = "plavaToolStripMenuItem";
+            this.plavaToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.plavaToolStripMenuItem.Text = "plava";
+            // 
+            // zelenaToolStripMenuItem
+            // 
+            this.zelenaToolStripMenuItem.Name = "zelenaToolStripMenuItem";
+            this.zelenaToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.zelenaToolStripMenuItem.Text = "zelena";
+            // 
+            // žutaToolStripMenuItem
+            // 
+            this.žutaToolStripMenuItem.Name = "žutaToolStripMenuItem";
+            this.žutaToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.žutaToolStripMenuItem.Text = "žuta";
+            // 
+            // toolStripMenuItem2
+            // 
+            this.toolStripMenuItem2.Name = "toolStripMenuItem2";
+            this.toolStripMenuItem2.Size = new System.Drawing.Size(180, 22);
+            this.toolStripMenuItem2.Text = "1";
+            // 
+            // toolStripMenuItem3
+            // 
+            this.toolStripMenuItem3.Name = "toolStripMenuItem3";
+            this.toolStripMenuItem3.Size = new System.Drawing.Size(180, 22);
+            this.toolStripMenuItem3.Text = "5";
+            // 
+            // toolStripMenuItem4
+            // 
+            this.toolStripMenuItem4.Name = "toolStripMenuItem4";
+            this.toolStripMenuItem4.Size = new System.Drawing.Size(180, 22);
+            this.toolStripMenuItem4.Text = "10";
+            // 
+            // Paint
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Paint";
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem olovkaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem bojaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem crvenaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem plavaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem zelenaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem žutaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem gumicaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem velicinaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem2;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem3;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem4;
+        private System.Windows.Forms.ToolStripMenuItem uveziFotografijuToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem sačuvajToolStripMenuItem;
+    }
+}
+
